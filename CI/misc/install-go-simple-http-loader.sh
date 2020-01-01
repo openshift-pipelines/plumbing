c2842e6ce5b769a1196d98e7d37d71d14dfe0900
Chmouel Boudjnah <chmouel@redhat.com>
2020-01-01 15:25:06 +0100

    No slashes
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- CI/misc/install-go-simple-http-loader.sh
+++ CI/misc/install-go-simple-http-loader.sh
@@ -2,5 +2,5 @@
 # Install GO SIMPLE Uploader, this assumes you have checked it out in GOPATH
 
 cd $HOME/GIT/go/src/github.com/chmouel/go-simple-uploader/openshift
-oc new-project osinstall 2>/dev/null || oc project osinstall
+oc project osinstall 2>/dev/null || oc new-project osinstall
 make deploy
