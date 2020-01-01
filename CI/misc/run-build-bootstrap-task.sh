7b464e448e719d2f3ac414414869d67e120f1053
Chmouel Boudjnah <chmouel@redhat.com>
2020-01-01 20:53:45 +0100

    No krec here...

--- CI/misc/run-build-bootstrap-task.sh
+++ CI/misc/run-build-bootstrap-task.sh
@@ -2,9 +2,11 @@
 # Shortcut to run the build bootstrap, just for giggles
 set -eu
 
-oc delete taskrun --all;
+kubectl delete taskrun --all;
 
-krec $(git rev-parse --show-toplevel)/CI/tasks/bootstrap/build-tektoncd-pipeline.yaml 
+for i in delete create;do
+    kubectl ${i} -f $(git rev-parse --show-toplevel)/CI/tasks/bootstrap/build-tektoncd-pipeline.yaml
+done
 
 tkn task start build-tektoncd-pipeline-and-push --showlog \
 	--param UPLOADER_HOST=http://$(oc get route -n osinstall uploader -o jsonpath={.spec.host}) \
