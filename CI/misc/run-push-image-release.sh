60146b85c1da8ebdbb7a5a8fd134b2cd1b925fa1
Chmouel Boudjnah <chmouel@redhat.com>
2020-01-10 13:02:27 +0100

    Add runner to shortcut in misc

---
+++ CI/misc/run-push-image-release.sh
@@ -0,0 +1,15 @@
+set -eu
+
+git push
+
+kubectl get -l "tekton.dev/task=repush-images-releases" tr -o name|xargs kubectl delete
+
+kubectl delete -f $(git rev-parse --show-toplevel)/CI/tasks/bootstrap/push-images-release.yaml 2>/dev/null || true
+kubectl create -f $(git rev-parse --show-toplevel)/CI/tasks/bootstrap/push-images-release.yaml
+
+
+tkn task start repush-images-releases --showlog \
+    --param UPLOADER_HOST=$(grep host ~/.uploader.cfg|sed 's/host=//') \
+	-i plumbing-git=plumbing-git \
+    -i tektoncd-pipeline-git=tektoncd-pipeline-git \
+    --serviceaccount builder
