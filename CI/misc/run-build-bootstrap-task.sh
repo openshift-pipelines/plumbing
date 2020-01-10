2bc53aa173de0bf938ecf4b3ed2a4b80c324de14
Chmouel Boudjnah <chmouel@redhat.com>
2020-01-10 10:58:11 +0100

    Let's make sure to git push

--- CI/misc/run-build-bootstrap-task.sh
+++ CI/misc/run-build-bootstrap-task.sh
@@ -2,6 +2,8 @@
 # Shortcut to run the build bootstrap, just for giggles
 set -eu
 
+git push
+
 kubectl get -l "tekton.dev/task=build-tektoncd-pipeline-and-push" tr -o name|xargs kubectl delete
 
 kubectl delete -f $(git rev-parse --show-toplevel)/CI/tasks/bootstrap/build-tektoncd-pipeline.yaml 2>/dev/null || true
